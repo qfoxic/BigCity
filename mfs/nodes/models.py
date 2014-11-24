84cc213bb230bee6f266d2cfc859a3b2596f577f
Volodymyr Paslavskyy <foxandkamarus@gmail.com>
2014-11-24 08:37:20 -0800

    Fixe dminor issue

--- mfs/nodes/models.py
+++ mfs/nodes/models.py
@@ -87,7 +87,7 @@ class GeoResource(Resource):
         self.geo_location = address_to_geo(*args)
 
     meta = {
-        'indexes': [("geo_location", "2dsphere"),]
+        'indexes': [[("location", "2dsphere"),]]
     }
 
 

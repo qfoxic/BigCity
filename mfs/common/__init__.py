7b505ace9eba8ac7a02c771b7a397b2b269b51f0
Volodymyr Paslavskyy <foxandkamarus@gmail.com>
2015-03-16 08:43:11 +0200

    Fixed root username

--- mfs/common/__init__.py
+++ mfs/common/__init__.py
@@ -5,7 +5,7 @@ from mfs.common.constants import ADMIN_GROUP
 # Create super user root.
 try:
     grp, _ = Group.objects.get_or_create(name=ADMIN_GROUP)
-    usr = get_user_model().objects.create_superuser('root', 'root', 'QAZqaz1983!@#$%^&*()_+')
+    usr = get_user_model().objects.create_superuser('root@mfs.com', 'root@mfs.com', 'QAZqaz1983!@#$%^&*()_+')
     usr.groups.add(grp)
 except:
     pass

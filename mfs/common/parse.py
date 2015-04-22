817c84efdd21036d53c19604a9628b862db5c89c
Volodymyr Paslavskyy <workforum@ukr.net>
2015-04-22 07:04:13 -0700

    Added pyparsing and some examples

--- mfs/common/parse.py
+++ mfs/common/parse.py
@@ -1,3 +1,250 @@
+# simpleSQL.py
+#
+# simple demo of using the parsing library to do simple-minded SQL parsing
+# could be extended to include where clauses etc.
+#
+# Copyright (c) 2003, Paul McGuire
+#
+from pyparsing import Literal, CaselessLiteral, Word, Upcase, delimitedList, Optional, \
+    Combine, Group, alphas, nums, alphanums, ParseException, Forward, oneOf, quotedString, \
+    ZeroOrMore, restOfLine, Keyword
+
+def test( str ):
+    print str,"->"
+    try:
+        tokens = simpleSQL.parseString( str )
+        print "tokens = ",        tokens
+        print "tokens.columns =", tokens.columns
+        print "tokens.tables =",  tokens.tables
+        print "tokens.where =", tokens.where
+    except ParseException, err:
+        print " "*err.loc + "^\n" + err.msg
+        print err
+    print
+
+
+# define SQL tokens
+selectStmt = Forward()
+selectToken = Keyword("select", caseless=True)
+fromToken   = Keyword("from", caseless=True)
+
+ident          = Word( alphas, alphanums + "_$" ).setName("identifier")
+columnName     = Upcase( delimitedList( ident, ".", combine=True ) )
+columnNameList = Group( delimitedList( columnName ) )
+tableName      = Upcase( delimitedList( ident, ".", combine=True ) )
+tableNameList  = Group( delimitedList( tableName ) )
+
+whereExpression = Forward()
+and_ = Keyword("and", caseless=True)
+or_ = Keyword("or", caseless=True)
+in_ = Keyword("in", caseless=True)
+
+E = CaselessLiteral("E")
+binop = oneOf("= != < > >= <= eq ne lt le gt ge", caseless=True)
+arithSign = Word("+-",exact=1)
+realNum = Combine( Optional(arithSign) + ( Word( nums ) + "." + Optional( Word(nums) )  |
+                                                         ( "." + Word(nums) ) ) +
+            Optional( E + Optional(arithSign) + Word(nums) ) )
+intNum = Combine( Optional(arithSign) + Word( nums ) +
+            Optional( E + Optional("+") + Word(nums) ) )
+
+columnRval = realNum | intNum | quotedString | columnName # need to add support for alg expressions
+whereCondition = Group(
+    ( columnName + binop + columnRval ) |
+    ( columnName + in_ + "(" + delimitedList( columnRval ) + ")" ) |
+    ( columnName + in_ + "(" + selectStmt + ")" ) |
+    ( "(" + whereExpression + ")" )
+    )
+whereExpression << whereCondition + ZeroOrMore( ( and_ | or_ ) + whereExpression )
+
+# define the grammar
+selectStmt      << ( selectToken +
+                   ( '*' | columnNameList ).setResultsName( "columns" ) +
+                   fromToken +
+                   tableNameList.setResultsName( "tables" ) +
+                   Optional( Group( CaselessLiteral("where") + whereExpression ), "" ).setResultsName("where") )
+
+simpleSQL = selectStmt
+
+# define Oracle comment format, and ignore them
+oracleSqlComment = "--" + restOfLine
+simpleSQL.ignore( oracleSqlComment )
+
+
+test( "SELECT * from XYZZY, ABC" )
+test( "select * from SYS.XYZZY" )
+test( "Select A from Sys.dual" )
+test( "Select A,B,C from Sys.dual" )
+test( "Select A, B, C from Sys.dual" )
+test( "Select A, B, C from Sys.dual, Table2   " )
+test( "Xelect A, B, C from Sys.dual" )
+test( "Select A, B, C frox Sys.dual" )
+test( "Select" )
+test( "Select &&& frox Sys.dual" )
+test( "Select A from Sys.dual where a in ('RED','GREEN','BLUE')" )
+test( "Select A from Sys.dual where a in ('RED','GREEN','BLUE') and b in (10,20,30)" )
+test( "Select A,b from table1,table2 where table1.id eq table2.id -- test out comparison operators" )
+
+"""
+Test output:
+>pythonw -u simpleSQL.py
+SELECT * from XYZZY, ABC ->
+tokens =  ['select', '*', 'from', ['XYZZY', 'ABC']]
+tokens.columns = *
+tokens.tables = ['XYZZY', 'ABC']
+
+select * from SYS.XYZZY ->
+tokens =  ['select', '*', 'from', ['SYS.XYZZY']]
+tokens.columns = *
+tokens.tables = ['SYS.XYZZY']
+
+Select A from Sys.dual ->
+tokens =  ['select', ['A'], 'from', ['SYS.DUAL']]
+tokens.columns = ['A']
+tokens.tables = ['SYS.DUAL']
+
+Select A,B,C from Sys.dual ->
+tokens =  ['select', ['A', 'B', 'C'], 'from', ['SYS.DUAL']]
+tokens.columns = ['A', 'B', 'C']
+tokens.tables = ['SYS.DUAL']
+
+Select A, B, C from Sys.dual ->
+tokens =  ['select', ['A', 'B', 'C'], 'from', ['SYS.DUAL']]
+tokens.columns = ['A', 'B', 'C']
+tokens.tables = ['SYS.DUAL']
+
+Select A, B, C from Sys.dual, Table2    ->
+tokens =  ['select', ['A', 'B', 'C'], 'from', ['SYS.DUAL', 'TABLE2']]
+tokens.columns = ['A', 'B', 'C']
+tokens.tables = ['SYS.DUAL', 'TABLE2']
+
+Xelect A, B, C from Sys.dual ->
+^
+Expected 'select'
+Expected 'select' (0), (1,1)
+
+Select A, B, C frox Sys.dual ->
+               ^
+Expected 'from'
+Expected 'from' (15), (1,16)
+
+Select ->
+      ^
+Expected '*'
+Expected '*' (6), (1,7)
+
+Select &&& frox Sys.dual ->
+       ^
+Expected '*'
+Expected '*' (7), (1,8)
+
+>Exit code: 0
+"""
+
+
+#
+# simpleBool.py
+#
+# Example of defining a boolean logic parser using
+# the operatorGrammar helper method in pyparsing.
+#
+# In this example, parse actions associated with each
+# operator expression will "compile" the expression
+# into BoolXXX class instances, which can then
+# later be evaluated for their boolean value.
+#
+# Copyright 2006, by Paul McGuire
+# Updated 2013-Sep-14 - improved Python 2/3 cross-compatibility
+#
+from pyparsing import infixNotation, opAssoc, Keyword, Word, alphas
+
+# define classes to be built at parse time, as each matching
+# expression type is parsed
+class BoolOperand(object):
+    def __init__(self,t):
+        self.label = t[0]
+        self.value = eval(t[0])
+    def __bool__(self):
+        return self.value
+    def __str__(self):
+        return self.label
+    __repr__ = __str__
+    __nonzero__ = __bool__
+
+class BoolBinOp(object):
+    def __init__(self,t):
+        self.args = t[0][0::2]
+    def __str__(self):
+        sep = " %s " % self.reprsymbol
+        return "(" + sep.join(map(str,self.args)) + ")"
+    def __bool__(self):
+        return self.evalop(bool(a) for a in self.args)
+    __nonzero__ = __bool__
+    __repr__ = __str__
+
+class BoolAnd(BoolBinOp):
+    reprsymbol = '&'
+    evalop = all
+
+class BoolOr(BoolBinOp):
+    reprsymbol = '|'
+    evalop = any
+
+class BoolNot(object):
+    def __init__(self,t):
+        self.arg = t[0][1]
+    def __bool__(self):
+        v = bool(self.arg)
+        return not v
+    def __str__(self):
+        return "~" + str(self.arg)
+    __repr__ = __str__
+    __nonzero__ = __bool__
+
+TRUE = Keyword("True")
+FALSE = Keyword("False")
+boolOperand = TRUE | FALSE | Word(alphas,max=1)
+boolOperand.setParseAction(BoolOperand)
+
+# define expression, based on expression operand and
+# list of operations in precedence order
+boolExpr = infixNotation( boolOperand,
+    [
+    ("not", 1, opAssoc.RIGHT, BoolNot),
+    ("and", 2, opAssoc.LEFT,  BoolAnd),
+    ("or",  2, opAssoc.LEFT,  BoolOr),
+    ])
+
+
+if __name__ == "__main__":
+    p = True
+    q = False
+    r = True
+    tests = [("p", True),
+             ("q", False),
+             ("p and q", False),
+             ("p and not q", True),
+             ("not not p", True),
+             ("not(p and q)", True),
+             ("q or not p and r", False),
+             ("q or not p or not r", False),
+             ("q or not (p and r)", False),
+             ("p or q or r", True),
+             ("p or q or r and False", True),
+             ("(p or q or r) and False", False),
+            ]
+
+    print("p =", p)
+    print("q =", q)
+    print("r =", r)
+    print()
+    for t,expected in tests:
+        res = boolExpr.parseString(t)[0]
+        success = "PASS" if bool(res) == expected else "FAIL"
+        print (t,'\n', res, '=', bool(res),'\n', success, '\n')
+
+
+
 # <statement> ::= ( <select_stmt> | <describe_stmt> ) [';']
 #
 # <select_stmt> ::= SELECT <select_list> <from_clause> [<where_clause>] [<given_clause>] <additional_clauses>
